2569780e993ab3f8e2789cd52e7095ff832b5148
Fabiana Ferlin <38224433+bianavic@users.noreply.github.com>
2020-12-28 14:52:08 -0300

    fix conflicts, again

--- src/main/java/br/com/concrete/challenge/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.concrete.challenge.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(long exception) {
-        super(String.valueOf(exception));
-    }
-}
