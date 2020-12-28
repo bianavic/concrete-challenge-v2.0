9bb2f397190886172025a761beeb99f22c48e2bf
Fabiana Ferlin <38224433+bianavic@users.noreply.github.com>
2020-12-28 13:37:22 -0300

    create a demo class to test configuration

---
+++ src/main/java/com/registration/challenge/models/Demo.java
@@ -0,0 +1,20 @@
+package com.registration.challenge.models;
+
+public class Demo {
+
+    private String message;
+
+    public Demo() {}
+
+    public Demo(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
